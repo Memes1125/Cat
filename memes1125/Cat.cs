8845dc0c055cc918c1bf95288e3b18f418a7e454
Memes1125 <pokimonchik34@gmail.com>
2020-11-02 13:05:02 +1000

    Алексей Анатольевич , я не понял куда деть день рождения кошки

---
+++ memes1125/Cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace memes1125
+{
+    class Cat
+    {
+         public string name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Name ;
+        public DateTime birthday ;
+        public int GatAge;
+        
+       
+        public void MakeNoise()
+        {
+         
+            Console.WriteLine($"{Name} мяукает ");
+        }
+        
+    }
+}
---
+++ memes1125/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace memes1125
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.Name = "Marusia";
+            cat.MakeNoise();
+            
+
+
+
+
+            cat.GatAge = 30;
+            string name = cat.Name;
+            
+            Console.WriteLine($"Кошке по имени{cat.Name} уже {cat.GatAge} лет");
+            
+
+            
+        }
+    }
+}
