273e5614b67352fe7d9da474efd9ef7f27916a1c
Memes1125 <pokimonchik34@gmail.com>
2020-11-02 21:17:56 +1000

    Задание на 03.11.2020

--- memes1125/Cat.cs
+++ memes1125/Cat.cs
@@ -1,52 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace memes1125
 {
     class Cat
     {
 
-        public string name { get; set; }
-        public DateTime Birthday { get; set; }
-        public Cat() { string Name = name; DateTime birthday = Birthday; byte _hungryStatus = HungryStatus;}
-        public int GatAge;
-        private byte _hungryStatus;
+        byte _hungryStatus;
+        public Cat(string name, DateTime birthday)
+        {
+            Name = name;
+            Birthday = birthday;
+            Task.Run(LifeCircle);
+        }
+        public string Name { get; set; }
 
+        public void Makenois()
+        {
+            Console.WriteLine($"{Name} мяукает");
+        }
+        public DateTime Birthday { get; set; }
+        public int GatAge()
+        {
+            return (DateTime.Today - Birthday).Days / 365;
+        }
         public byte HungryStatus
         {
-
-            get
-            {
-                return _hungryStatus;
-            }
+            get { return _hungryStatus; }
             set
             {
-                if(value < 0)
+                if (value < 0)
+                {
                     _hungryStatus = 0;
-                if (value > 100)
+                }
+                else if (value > 100)
+                {
                     _hungryStatus = 100;
-
-
+                }
+                else
+                    _hungryStatus = value;
             }
-            
         }
-
-        void GetStatus()
+        public void GetStatus()
         {
-            Console.WriteLine($"{name}");
-            Console.WriteLine($"Возраст {GatAge}");
-            Console.WriteLine(_hungryStatus);
+            Console.WriteLine(Name);
+            Console.WriteLine($"Возраст {GatAge()}");
+            if(HungryStatus <= 10)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Кошка умерает от голода ");
+            }
+            else if (HungryStatus > 10 && HungryStatus <= 40)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Кошка очень голодна");
+            }
+            else if(HungryStatus > 40 && HungryStatus <= 70)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Кошка хочет кушать ");
+            }
+            else if(HungryStatus > 70 && HungryStatus <= 90)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Кошка не против перекусить ");
+            }
+            else if (HungryStatus > 90)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Кошка недавно поела ");
+            }
+            Console.ResetColor();
         }
-
-        public void MakeNoise()
-        {
-         
-            Console.WriteLine($"{name} мяукает ");
-        }
-        
-
       
+      async Task LifeCircle()
+        {
+            await Task.Delay(10000);
+            HungryStatus -= 10;
+            GetStatus();
+            if (HungryStatus == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Кошки нет в живых");
+            }
+            else
+                await LifeCircle();
+        }
+
+
+
     }
 }
